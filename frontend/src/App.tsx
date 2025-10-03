3d29eb27901b715d08997f1162ad4df038bb2adb
jaydeep-99o <jdgujar99o@gmail.com>
2025-10-03 16:11:47 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        🚀 React + Tailwind v4 is live!
+      </h1>
+    </div>
+  )
+}
